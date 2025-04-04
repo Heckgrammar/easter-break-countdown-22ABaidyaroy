699326f35257407a547909506b56822a0ed05711
Ananya Baidyaroy <22ABaidyaroy@heckgrammar.co.uk>
2025-04-04 10:15:13 +0100

    yyj

--- BlankCSDotNet8/Program.cs
+++ BlankCSDotNet8/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-           
+            bool valid = false;
+            while (valid == false)
+            {
+                DateTime current = DateTime.Now;
+                DateTime target = new DateTime(2025, 4, 4, 15, 15, 0);
+                TimeSpan difference = target - current;
+
+                Console.WriteLine(difference);
+                Thread.Sleep(1000);
+                
+                if (current == target)
+                {
+                    valid = true;
+                }
+            }
         }
     }
 }
